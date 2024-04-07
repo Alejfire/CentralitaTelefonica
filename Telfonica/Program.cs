077c96406128eae49fa1d852d7e8c24b38210268
Alejfire <alejandro.gomez.escoto@gmail.com>
2024-04-06 21:52:59 -0400

    first commit

--- Telfonica/Program.cs
+++ Telfonica/Program.cs
@@ -1,4 +1,8 @@
-﻿class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+class Program
 {
     static void Main(string[] args)
     {
@@ -7,41 +11,25 @@
 
         Centralita centralita = new Centralita();
 
-
-        LlamadaLocal llamadaLocal = new LlamadaLocal("767772", "185436", 104, 0.15);
+        LlamadaLocal llamadaLocal1 = new LlamadaLocal("767772", "185436", 104, 0.15);
         LlamadaLocal llamadaLocal2 = new LlamadaLocal("033398", "761655", 3310, 0.15);
         LlamadaLocal llamadaLocal3 = new LlamadaLocal("148633", "676363", 301, 0.15);
 
-        LlamadaProvincial llamadaProvincial = new LlamadaProvincial("822487", "198713", 152, 0.15, 0.20, 0.25, 5);
+        LlamadaProvincial llamadaProvincial1 = new LlamadaProvincial("822487", "198713", 152, 0.15, 0.20, 0.25, 5);
         LlamadaProvincial llamadaProvincial2 = new LlamadaProvincial("127634", "776536", 1708, 0.15, 0.20, 0.25, 5);
         LlamadaProvincial llamadaProvincial3 = new LlamadaProvincial("315777", "4543134", 340, 0.15, 0.20, 0.25, 5);
 
-
-
-        centralita.AgregarLlamada(llamadaLocal);
-        centralita.AgregarLlamada(llamadaLocal);
-
+        centralita.AgregarLlamada(llamadaLocal1);
         centralita.AgregarLlamada(llamadaLocal2);
-        centralita.AgregarLlamada(llamadaLocal2);
-
         centralita.AgregarLlamada(llamadaLocal3);
-        centralita.AgregarLlamada(llamadaLocal3);
-
-        centralita.AgregarLlamada(llamadaProvincial);
-        centralita.AgregarLlamada(llamadaProvincial);
-
+        centralita.AgregarLlamada(llamadaProvincial1);
         centralita.AgregarLlamada(llamadaProvincial2);
-        centralita.AgregarLlamada(llamadaProvincial2);
-
         centralita.AgregarLlamada(llamadaProvincial3);
-        centralita.AgregarLlamada(llamadaProvincial3);
-
 
         double totalLocal = centralita.CalcularTotalLocal();
         double totalProvincial = centralita.CalcularTotalProvincial();
 
-        Console.WriteLine($"    Hubo un total de {centralita.acum} llamadas. \n");
-
+        Console.WriteLine($"    Hubo un total de {centralita.Acumulador} llamadas. \n");
         Console.WriteLine($"    Total de llamadas locales: {totalLocal:C}");
         Console.WriteLine($"    Total de llamadas provinciales: {totalProvincial:C}");
         Console.WriteLine();
@@ -49,39 +37,31 @@
     }
 }
 
-
-
 public abstract class Llamada
 {
-
     protected string numOrigen;
     protected string numDestino;
-    protected double duracion;
 
-    public Llamada (string numOrigen, string numDestino, double duracion)
+    public Llamada(string numOrigen, string numDestino)
     {
         this.numOrigen = numOrigen;
         this.numDestino = numDestino;
-        this.duracion = duracion;
     }
-
-
-    public string NumDestino { get => numDestino; }
-    public string NumOrigen { get => numOrigen; }
-    
 
     public abstract double CalcularPrecio();
 }
 
-public class LlamadaProvincial:Llamada
+public class LlamadaProvincial : Llamada
 {
+    private double duracion;
     private double precio1;
     private double precio2;
     private double precio3;
     private int franja;
 
-    public LlamadaProvincial(string numOrigen, string numDestino, double duracion, double precio1, double precio2, double precio3, int franja) : base(numOrigen, numDestino, duracion)
+    public LlamadaProvincial(string numOrigen, string numDestino, double duracion, double precio1, double precio2, double precio3, int franja) : base(numOrigen, numDestino)
     {
+        this.duracion = duracion;
         this.precio1 = precio1;
         this.precio2 = precio2;
         this.precio3 = precio3;
@@ -94,50 +74,50 @@ public class LlamadaProvincial:Llamada
         {
             return duracion * precio1;
         }
-        else if (duracion <= franja*2)
+        else if (duracion <= franja * 2)
         {
             return duracion * precio2;
         }
-
-        return duracion * precio3;
-
+        else
+        {
+            return duracion * precio3;
+        }
     }
-
 }
 
 public class LlamadaLocal : Llamada
 {
+    private double duracion;
     private double precio;
 
-    public LlamadaLocal(string numOrigen, string numDestino, double duracion, double precio):base(numOrigen, numDestino, duracion)
+    public LlamadaLocal(string numOrigen, string numDestino, double duracion, double precio) : base(numOrigen, numDestino)
     {
+        this.duracion = duracion;
         this.precio = precio;
     }
 
     public override double CalcularPrecio()
     {
-        return precio * duracion;
+        return duracion * precio;
     }
-
 }
 
 public class Centralita
 {
-    private int cont;
-    public int acum { get; private set; }
-
     private List<Llamada> llamadas;
+
+    public int Acumulador { get; private set; }
 
     public Centralita()
     {
         llamadas = new List<Llamada>();
-        acum = 0;
+        Acumulador = 0;
     }
 
     public void AgregarLlamada(Llamada llamada)
     {
         llamadas.Add(llamada);
-        acum ++;
+        Acumulador++;
     }
 
     public double CalcularTotalLocal()
@@ -146,12 +126,10 @@ public class Centralita
 
         foreach (var llamada in llamadas)
         {
-
             if (llamada is LlamadaLocal)
             {
                 total += llamada.CalcularPrecio();
             }
-
         }
 
         return total;
@@ -163,12 +141,10 @@ public class Centralita
 
         foreach (var llamada in llamadas)
         {
-
             if (llamada is LlamadaProvincial)
             {
                 total += llamada.CalcularPrecio();
             }
-
         }
 
         return total;
--- Telfonica/obj/Debug/net8.0/Telfonica.AssemblyInfo.cs
+++ Telfonica/obj/Debug/net8.0/Telfonica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Telfonica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58374e8524c0e8dea9f350ab949b98cec1c7ff84")]
 [assembly: System.Reflection.AssemblyProductAttribute("Telfonica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Telfonica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
